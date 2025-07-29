e9c2c777ff53af63c11b4b3784ba8377070a90a0
Steve Korshakov <steve@korshakov.com>
2025-07-28 20:56:01 -0700

    fix: fix timeout

--- src/modules/watcher/awaitFileExist.ts
+++ src/modules/watcher/awaitFileExist.ts
@@ -2,7 +2,8 @@ import { access } from "fs/promises";
 import { delay } from "@/utils/time";
 
 export async function awaitFileExist(file: string, timeout: number = 10000) {
-    while (true) {
+    const startTime = Date.now();
+    while (Date.now() - startTime < timeout) {
         try {
             await access(file);
             return true;
