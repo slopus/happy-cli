f2c9ba954e24673b56a6be66312a1a314008feab
Steve Korshakov <steve@korshakov.com>
2025-08-14 21:39:13 -0700

    fix: disable autoupdater warning

--- scripts/claude_local_launcher.cjs
+++ scripts/claude_local_launcher.cjs
@@ -1,6 +1,9 @@
 const crypto = require('crypto');
 const fs = require('fs');
 
+// Disable autoupdater (never works really)
+process.env.DISABLE_AUTOUPDATER = '1';
+
 // Helper to write JSON messages to fd 3
 function writeMessage(message) {
     try {
