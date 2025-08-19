16a1ba4402254e487b2f0e8a207b7c85f090a6e9
nobody <nobody@localhost>
2025-08-19 12:18:09 -0700

    fix: windows installation, add smoke test to CI

--- bin/happy.cmd
+++ bin/happy.cmd
@@ -1,3 +1,3 @@
 @echo off
 set NODE_NO_WARNINGS=1
-node --no-warnings --no-deprecation "%~dp0\happy" %*
+node --no-warnings --no-deprecation "%~dp0\..\dist\index.mjs" %*
--- bin/happy.mjs
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env node --no-warnings --no-deprecation
-import "../dist/index.mjs";
